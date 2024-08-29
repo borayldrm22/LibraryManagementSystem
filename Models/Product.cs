2a544db43744382a8705bfd738420f80766cfbd2
ıtstajer <tstajer@TCH-MTD-5>
2024-08-29 10:27:32 +0300

    Proje dosyası ekle.

---
+++ Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kutuphane2.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [DisplayName("İsim")]
+        public string Name { get; set; } = "";
+        public string Brand { get; set; } = "";
+
+
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = "";
+
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = "";
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Kutuphane2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kutuphane2.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+        
+        }
+        
+        public DbSet<Product> Products { get; set; }    
+    }
+}
