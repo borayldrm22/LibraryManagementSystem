668d996a4fb36ff5e5e2a4c468548cf96b1bac4a
Bora <110013569+borayldrm22@users.noreply.github.com>
2024-08-29 10:31:00 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿INSERT INTO Products (Name,Brand, Category, Price, Description, ImageFileName, CreatedAt)
+VALUES
+('Ah Min-el Aşk','Book', 'Şiir', 75, 'Ferit Edgü nün kaleminden, Semiha Berksoy un çizimleriyle süslenmiş bir şiir kitabı.', '179633_9786051714851.jpg', GETDATE()),
+('Kürk Mantolu Madonna','Book',  'Roman', 50, 'Sabahattin Ali nin unutulmaz eseri, aşk ve yalnızlık üzerine derin bir roman.', 'images (1).jfif', GETDATE()),
+('Otuz Beş Yaş', 'Book', 'Şiir', 60, 'Cahit Sıtkı Tarancı nın, hayatın dönüm noktalarına dokunan etkileyici şiirleri.', '51IsundOKVL._AC_UF894,1000_QL80_.jpg',GETDATE()),
+('Seyyahı Şiir', 'Book', 'Şiir', 70, 'Turgut Uyar dan derin ve etkileyici şiirler.', 'images.jfif', GETDATE()),
+('Göğe Bakma Durağı', 'Book', 'Şiir', 65, 'Cemal Süreya nın en bilinen şiirlerinden oluşan seçki.', '110000693315177.jpg', GETDATE()),
+('Sevda Sözleri', 'Book', 'Şiir', 80, 'Türk edebiyatının önemli şairlerinden aşk dolu dizeler.', 'en-cok-satan-siir-kitabi-1666692073.jpg', GETDATE()),
+('Deadpool', 'Book', 'Çizgi Roman', 40, 'Deadpool un mizah dolu ve aksiyonlu maceraları.', '9447591968818.jpg', GETDATE()),
+('Mickey Mouse', 'Book', 'Çizgi Roman', 30, 'Mickey Mouse un eğlenceli ve klasikleşmiş hikayeleri.', '292.jpg',GETDATE()),
+('Dracula', 'Book', 'Çizgi Roman', 50, 'Bram Stoker ın efsanevi karakteri Dracula nın çizgi roman uyarlaması.', '0000000526652-1.jpg', GETDATE()),
+('Justice League','Book',  'Çizgi Roman', 55, 'Dünyanın en güçlü kahramanlarının bir araya geldiği Justice League serisi.', 'justice-league.jpeg', GETDATE()),
+('Marvel Gizli Savaşlar','Book',  'Çizgi Roman', 45, 'Marvel evreninin en büyük gizli savaşlarının anlatıldığı çizgi roman serisi.', '9447596195890.jpg', GETDATE()),
+('Suç ve Ceza','Book',  'Roman', 90, 'Dostoyevski nin unutulmaz başyapıtı, insan ruhunun derinliklerine inen bir roman.', '0000000222780-1.jpg', GETDATE()),
+('Nietzsche Ağladığında', 'Book', 'Roman', 85, 'Irvin D. Yalom dan, felsefe ve psikolojinin iç içe geçtiği derin bir roman.', 'wi_500 (1).jfif', GETDATE()),
+('Simyacı', 'Book', 'Roman', 60, 'Paulo Coelho nun insanın kendi kaderini bulma yolculuğunu anlattığı unutulmaz roman.', 'simyaci.jpeg', GETDATE()),
+('Şeker Portakalı', 'Book', 'Roman', 50, 'Jose Mauro de Vasconcelos tan, küçük Zeze nin büyüme hikayesini anlatan bir klasik.', '0002064214001-1.jpg', GETDATE());
